08f24e39d35bfed1b0f8161e205a8bf1a7627820
yifanwow <yifanovo@gmail.com>
2024-03-18 01:33:16 -0400

    实现通过openhardwaremonitor的WMI接口访问所有control的端口并进行输出。

--- fanctrl_program/Program.cs
+++ fanctrl_program/Program.cs
@@ -14,20 +14,18 @@ class Program
         // 执行查询
         ManagementObjectCollection collection = searcher.Get();
 
-        // 遍历传感器
+        // 遍历控制器传感器
         foreach (ManagementObject obj in collection)
         {
-            // 检查传感器是否为风扇并且有转速信息
             if (
-                obj["SensorType"].ToString() == "Fan"
+                obj["SensorType"].ToString() == "Control"
                 && obj["Value"] != null
                 && obj["Value"].ToString() != "0"
             )
             {
-                string sensorName = obj["Name"]?.ToString() ?? "Unknown"; //在转换之前添加 null 检查或使用 null 合并运算符（?.）来安全地访问这些值
-                string fanSpeed = obj["Value"]?.ToString() ?? "Unknown";
-
-                Console.WriteLine("风扇名称: " + sensorName + ", 当前转速: " + fanSpeed + " RPM");
+                string sensorName = obj["Name"]?.ToString() ?? "Unknown";
+                string powerValue = obj["Value"]?.ToString() ?? "Unknown";
+                Console.WriteLine("端口名称: " + sensorName + ", 当前功率: " + powerValue + " %");
             }
         }
 
